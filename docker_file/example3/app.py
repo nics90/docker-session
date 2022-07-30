1caf94755cd0b397ae076153703d8e3a6071046b
nics90 <nics90@intellipaat.com>
2022-07-30 18:21:14 +0000

    Dockerfile and Docker Compose

---
+++ docker_file/example3/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+        
+@app.route("/devops")
+def devops():
+    return "Hello, DevOps"
+            
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5000)
